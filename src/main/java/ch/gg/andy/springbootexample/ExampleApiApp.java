5fe2fbbf2a8ec2c83f0ec8f5b8075eb443ec7d5d
Duckolyte <example@example.com>
2020-07-24 22:28:14 +0200

    Made a basic spring boot application setup.

---
+++ src/main/java/ch/gg/andy/springbootexample/ExampleApiApp.java
@@ -0,0 +1,12 @@
+package ch.gg.andy.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApiApp.class, args);
+    }
+}
